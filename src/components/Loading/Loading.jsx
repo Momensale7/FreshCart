844e131749400064c6f1ecf003ad103195891517
momen <momensalah2468@gmail.com>
2024-06-12 01:57:47 +0300

    first commit

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,15 +1,17 @@
 import { Player } from '@lottiefiles/react-lottie-player';
+import '@dotlottie/player-component'
 export default function Loading() {
   return (
     <div>
       <Player
-        src='./src/components/Loading/Animation - 1717840785142.json'
+        src='git/anm.json'
         className="player"
         loop
         autoplay
-        // speed={5000}
         style={{ height: '300px', width: '300px' }}
       />
+      <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+      <dotlottie-player src="https://lottie.host/8b36eae2-4442-454d-b6ff-0e47e5505683/eZvDQp34rZ.json" background="transparent" speed="1" style={{width: 300+"px",height: 300+"px"}} direction="1" playMode="normal" loop controls autoplay></dotlottie-player>
     </div>
   )
 }
