b2a590c16340d01e111ca2ffcba500b817339cdd
momen <momensalah2468@gmail.com>
2024-06-12 11:56:48 +0300

    animation

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,7 +1,15 @@
 import { Player } from '@lottiefiles/react-lottie-player';
 import '@dotlottie/player-component'
 export default function Loading() {
-  return (
-    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> <dotlottie-player src="https://lottie.host/8b36eae2-4442-454d-b6ff-0e47e5505683/eZvDQp34rZ.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
-  )
+  return <>
+  <div className='flex justify-center items-center'>
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module">
+
+    </script>
+     <dotlottie-player src="https://lottie.host/8b36eae2-4442-454d-b6ff-0e47e5505683/eZvDQp34rZ.json" background="transparent" speed="1" style={{width: 300+'px', height: 300+'px'}} loop autoplay>
+
+     </dotlottie-player>
+     </div>
+  </>
+  
 }
--- src/components/WichList/WichList.jsx
+++ src/components/WichList/WichList.jsx
@@ -1,21 +1,39 @@
 import { useContext, useEffect, useState } from "react";
 import { WishContext } from "../../Context/WishContext";
 import Wish from "../Wish/Wish";
+import Loading from "../Loading/Loading";
 
-export default function WichList() {
-  const {getallwish }= useContext(WishContext)
-  let [wishData,setWishData]=useState()
-  // setData(getallwish)
-  async function getAllWish(){
-    const {data}= await getallwish();
-    console.log(data)
-    setWishData(data)
+export default function WishList() {
+  const { getallwish } = useContext(WishContext);
+  const [wishData, setWishData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function getAllWish() {
+    const { data } = await getallwish();
+    console.log(data);
+    setWishData(data);
   }
-  useEffect(()=>{
-    getAllWish()
-  },[])
-  return wishData?.map((wishData,index)=>{
-    return <Wish  key={index } getAllWish={getAllWish} wishData={wishData}/>
-  })
-  
+
+  useEffect(() => {
+    const delay = 4000; // 2 seconds
+    setIsLoading(true);
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, delay);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  useEffect(() => {
+    getAllWish();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return wishData?.map((wishData, index) => (
+    <Wish key={index} getAllWish={getAllWish} wishData={wishData} />
+  ));
 }
