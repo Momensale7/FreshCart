e7088ab45dbe98aa872a29b4327cbb4bcc6f0904
Momensale7 <123418614+Momensale7@users.noreply.github.com>
2024-06-12 03:02:05 +0300

    Update Loading.jsx

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,16 +2,6 @@ import { Player } from '@lottiefiles/react-lottie-player';
 import '@dotlottie/player-component'
 export default function Loading() {
   return (
-    <div>
-      <Player
-        src='https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs'
-        className="player"
-        loop
-        autoplay
-        style={{ height: '300px', width: '300px' }}
-      />
-      <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
-      <dotlottie-player src="https://lottie.host/8b36eae2-4442-454d-b6ff-0e47e5505683/eZvDQp34rZ.json" background="transparent" speed="1" style={{width: 300+"px",height: 300+"px"}} direction="1" playMode="normal" loop controls autoplay></dotlottie-player>
-    </div>
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> <dotlottie-player src="https://lottie.host/8b36eae2-4442-454d-b6ff-0e47e5505683/eZvDQp34rZ.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
   )
 }
